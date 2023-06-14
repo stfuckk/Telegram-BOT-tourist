91489bcd7d0503c18065335c1bcdfe1a94d0c0c1
stfackk <denkis2205@gmail.com>
2023-06-14 19:27:47 +0700

    Stable ver with /weather (отказ от /guide)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
 from weather import get_weather
-#from guide import get_guide_list
+from guide import get_guide_list
 
 TOKEN = open("TOKEN.txt").read().split()[0]
 
@@ -13,7 +13,7 @@ async def start(update, context) -> None:
         [KeyboardButton('/about')]
     ], resize_keyboard=True)
     await update.message.reply_text(
-        f'**Привет! Я диня - бот, который предоставит полезную информацию для твоего путешествия!**\n'
+        f'***Привет! Я диня - бот, который предоставит полезную информацию для твоего путешествия!***\n'
         f'Напиши /help, чтобы узнать мои команды, так же, ты можешь воспользоваться клавиатурой.',
         reply_markup=keyboard, parse_mode="Markdown")
 
@@ -21,14 +21,14 @@ async def start(update, context) -> None:
 # обработчик команды /help
 async def help(update, context) -> None:
     await update.message.reply_text(f'Мои команды:\n'
-                                    f'**/weather [город]** - получить прогноз погоды для заданного города;\n'
-                                    f'**/guide [место] ** - получить список интересных мест для заданного места.'
-                                    f'(напишите "город страна" для точного поиска)\n', parse_mode="Markdown")
+                                    f'***/weather [город]*** - получить прогноз погоды для заданного города;\n'
+                                    f'***/guide [место]*** - получить список интересных мест для заданного места\n'
+                                    f'(напишите ***город и страну*** для точного поиска)\n', parse_mode="Markdown")
 
 
 # обработчик команды /about
 async def about(update, context) -> None:
-    await update.message.reply_text(f'**Учебный проект по созданию чат-бота в telegram,\nv1.0-stable**',
+    await update.message.reply_text(f'***Учебный проект по созданию чат-бота в telegram,\nv1.0-stable***',
                                     parse_mode="Markdown")
 
 
@@ -36,10 +36,11 @@ async def about(update, context) -> None:
 async def echo(update, context) -> None:
     # обработка try, чтобы избежать выбрасывания exception в случае редактирования сообщения пользователем
     try:
-        await update.message.reply_text(f'Я не понимаю, что вы говорите. Попробуйте другую команду из **/help**.',
+        await update.message.reply_text(f'Я не понимаю, что вы говорите. Попробуйте другую команду из ***/help***.',
                                         parse_mode="Markdown")
     except:
         return
+
 
 # обработчики основных команд
 # weather
@@ -47,7 +48,7 @@ async def weather(update, context) -> None:
     try:
         city = update.message.text.split()[1]
     except IndexError:
-        await update.message.reply_text('Напишите команду в виде "**/weather [город]**", пожалуйста.',
+        await update.message.reply_text('Напишите команду в виде "***/weather [город]***", пожалуйста.',
                                         parse_mode="Markdown")
         return
     weather_data = await get_weather(city)
@@ -57,13 +58,20 @@ async def weather(update, context) -> None:
 # guide
 async def guide(update, context) -> None:
     try:
-        place = update.message.text.split()[1:]
+        place = update.message.text.split()[1]
+        place = ' '.join(update.message.text.split()[1:])
     except IndexError:
-        await update.message.reply_text('Напишите команду в виде "**/guide [место]**", пожалуйста.',
+        await update.message.reply_text('Напишите команду в виде "***/guide [место]***", пожалуйста.',
                                         parse_mode="Markdown")
         return
+
+    await update.message.reply_text('***Ожидайте!***', parse_mode="Markdown")
     guide_data = await get_guide_list(place)
-    await update.message.reply_text(guide_data, parse_mode="Markdown")
+    if isinstance(guide_data, str):
+        await update.message.reply_text(guide_data, parse_mode="Markdown")
+    else:
+        for showplace in guide_data:
+            await update.message.reply_photo(photo=showplace[0], caption=showplace[1], parse_mode="Markdown")
 
 
 def main():
@@ -76,7 +84,8 @@ def main():
     # /weather
     application.add_handler(CommandHandler('weather', weather))
 
-    # /
+    # /guide
+    application.add_handler(CommandHandler('guide', guide))
 
     application.add_handler(MessageHandler(filters.TEXT, echo))
     application.run_polling(print('Бот готов к работе!'))
--- weather.py
+++ weather.py
@@ -3,7 +3,6 @@ import requests
 # Задаем URL и API-KEY OpenWeatherMap
 URL = 'http://api.openweathermap.org/data/2.5/weather'
 API_KEY = open("TOKEN.txt").read().split()[1]
-print(API_KEY)
 
 
 # Получаем погоду в виде текста
@@ -26,11 +25,11 @@ async def get_weather(city: str) -> str:
         wind = data['wind']['speed']
         # прогноз на ближайшие дни
         forecast = data['weather'][0]['description']
-        return (f'**ПОГОДА В {city.upper()}**\n'
-                f'Температура: **{temp}°C**\n'
-                f'Ощущается как: **{temp_fell}°C**\n'
-                f'Влажность: **{humidity}%**\n'
-                f'Скорость ветра: **{wind}м/c**\n'
-                f'Прогноз на ближайшие дни: **{forecast}**')
+        return (f'**ПОГОДА В {city.upper()}***\n'
+                f'Температура: ***{temp}°C***\n'
+                f'Ощущается как: ***{temp_fell}°C***\n'
+                f'Влажность: ***{humidity}%***\n'
+                f'Скорость ветра: ***{wind}м/c***\n'
+                f'Прогноз на ближайшие дни: ***{forecast}***')
     else:
         return 'Не удалось получить данные о погоде. Попробуйте ещё раз.'
